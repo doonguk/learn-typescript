07425db1d62133354935fdcdd868a839de1d807d
doonguk <mbxd1@naver.com>
2020-01-09 02:52:26 +0900

    prop type func, option

--- start-react-with-ts/src/App.tsx
+++ start-react-with-ts/src/App.tsx
@@ -3,10 +3,13 @@ import Hello from "./Hello";
 import Nice from './Nice'
 
 const App: React.FC = () => {
+    const handleClick = (name: string) => {
+        console.log('name name', name)
+    }
     return (
         <>
             <Hello name="donguk" say="thank you"/>
-            <Nice/>
+            <Nice func={handleClick}/>
         </>
     );
 }
--- start-react-with-ts/src/Nice.tsx
+++ start-react-with-ts/src/Nice.tsx
@@ -3,17 +3,23 @@ import React from 'react'
 type niceProps = {
     name: string
     say: string
+    options?: string
+    func: (name: string) => void
 }
 
-const Nice = ({name, say}: niceProps) => {
+const Nice = ({name, say, options, func}: niceProps) => {
     return(
-        <div>Hello My Name is {name}! {say}</div>
+        <>
+            <div>Hello My Name is {name}! {say}</div>
+            {options && <div>Here is {options}</div>}
+            <button onClick={()=>func(name)}>Click</button>
+        </>
     )
 }
 
 Nice.defaultProps = {
     name : 'donguk',
-    say : 'thx thx'
+    say : 'thx thx',
 }
 
 export default Nice

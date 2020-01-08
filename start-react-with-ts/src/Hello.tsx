ed9bd0fd64681eeb987021628e5e4c5adb16f3d1
doonguk <mbxd1@naver.com>
2020-01-09 02:34:51 +0900

    React.FC

---
+++ start-react-with-ts/src/Hello.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+type helloProps = {
+    name: string
+    say: string
+}
+
+const Hello: React.FC<helloProps> = ({name, say}) => {
+ return(
+        <div>Hello My Name is {name}! {say}</div>
+    )
+}
+
+Hello.defaultProps = {
+    say : 'thank you'
+}
+
+export default Hello
